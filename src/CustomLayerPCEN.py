f7a56a7a2b4e8d293d1cb3e9bda7329ed22dab51
Bastien Dussard <bastien.dussard@gmail.com>
2021-11-18 18:56:08 +0100

    New fixes on PCEN

--- src/CustomLayerPCEN.py
+++ src/CustomLayerPCEN.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
 import tensorflow.keras.backend as K
+from tensorflow.python.framework import tensor_shape
 
 class CustomLayerPCEN(tf.keras.layers.Layer):
 
@@ -23,20 +24,36 @@ class CustomLayerPCEN(tf.keras.layers.Layer):
 
     s = self.s
     eps = self.eps
-    outputs=np.copy(data)
-   
-    #Loop on each feature (corresponding to each frequency in the mfcc) and each timestep 
-    for i in np.size(data, 1):
+    #res = tf.identity(data)
+    res = np.zeros([int(data.shape[-2]), int(data.shape[-1])])
+    print(data.shape)
+    #Loop on each feature (corresponding to each frequency in the mfcc) and each timestep
+    for i in range(data.shape[-2]):
         prec_mean, current_mean = 0, 0
-        for j in np.size(data, 2):
+        for j in range(data.shape[-1]):
             #First timestep, there is no previous value, so we set prec_mean to zero
             if j == 0:
                 prec_mean = 0
             else:
                 prec_mean = current_mean
             #Compute the current mean based on the previous mean and the current value
-            current_mean = (1-s)*prec_mean + s*data(i,j)
+            current_mean = (1-s)*prec_mean + s*data[i,j]
             #Compute the value of the PCEN for each t, f
-            outputs(i,j) = (((data(i,j)/((eps+current_mean)**self.alpha))+self.delta)**self.r)-self.delta**self.r
-     
-    return outputs
+            
+            res[i,j] = (((data[i,j]/((eps+current_mean)**self.alpha))+self.delta)**self.r)-self.delta**self.r
+
+    return res
+
+if __name__ == '__main__':
+
+    data = np.random.uniform(1,50, size=(20,10))
+
+    test_model = CustomLayerPCEN()
+    
+    #output_model = test_model(data)
+    input = tf.keras.layers.Input(shape=data.shape)
+    model = Model(input, test_model(input) )
+    model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', run_eagerly=True)
+    history = model.fit(data, epochs=3, verbose = 1)
+    print(model.layers[-1].weights)
+    #model.summary()
